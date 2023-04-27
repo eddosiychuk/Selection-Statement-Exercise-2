fda5d31a98cf14e40a5a93041a347a7e89136c7f
Edward Osiychuk <edwardosiychuk@Edwards-Air.lan>
2023-04-27 13:45:26 -0600

    finished exerecise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Whst is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a hard subject!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is a interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Nice!, I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+            }
         }
     }
 }
